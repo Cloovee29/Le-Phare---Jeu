bbbce3d567f752a77da16491ac5075143c936984
Sacha <noirjean.sacha@gmail.com>
2025-11-14 10:40:48 +0100

    Créer des scripts pour le texte à trou

--- Le Phare V3/Assets/Scripts/MotScript.cs
+++ Le Phare V3/Assets/Scripts/MotScript.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
 
 public class MotScript : MonoBehaviour
 {
+
+ public string word; // le mot utilisé pour remplir le journal
+    private Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvas = GetComponentInParent<Canvas>();
+        GetComponentInChildren<TextMeshProUGUI>().text = word;
     }
 
     // Update is called once per frame
@@ -13,4 +24,21 @@ public class MotScript : MonoBehaviour
     {
         
     }
+
+
+     public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+    }
+
 }
---
+++ Le Phare V3/Assets/Scripts/TrouScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TrouScript : MonoBehaviour
+{
+
+ public int slotID; // numéro du trou dans le texte
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     public void OnDrop(PointerEventData eventData)
+    {
+        var dragged = eventData.pointerDrag.GetComponent<MotScript>();
+
+        if (dragged != null)
+        {
+            JournalTexteScript.Instance.FillSlot(slotID, dragged.word);
+        }
+    }
+}
