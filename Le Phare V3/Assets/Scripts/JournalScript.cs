d1be548ded7288805900e5303812884e42086d46
Sacha <noirjean.sacha@gmail.com>
2025-11-14 12:16:38 +0100

    page 2 du journal début

--- Le Phare V3/Assets/Scripts/JournalScript.cs
+++ Le Phare V3/Assets/Scripts/JournalScript.cs
@@ -8,6 +8,8 @@ public Vector3 fermetureJournal;
 public bool journalOuvert;
 public GameObject motADrag;
 
+public Sprite newSprite; 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,11 +21,20 @@ public GameObject motADrag;
     // Update is called once per frame
     void Update()
     {
-        
+       
+     if (Input.GetKeyDown(KeyCode.P)&& journalOuvert){
+         GetComponent<SpriteRenderer>().sprite = newSprite;
+            }
+
+
      if (Input.GetKeyDown(KeyCode.J) && journalOuvert==false){
            transform.position += ouvertureJournal;
            journalOuvert = true;
            motADrag.SetActive(true);
+
+           
+
+
      }else if (Input.GetKeyDown(KeyCode.J) && journalOuvert==true){
          transform.position += fermetureJournal;
            journalOuvert = false;
