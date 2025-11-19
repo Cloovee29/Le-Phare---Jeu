ad75b816976f8057f3415d089032d9357c8727d6
Cloovee29 <clothilde.moulis@gmail.com>
2025-11-19 19:13:44 +0100

    Début Raycast (inventaire utilisation) marche pas encore

--- Le Phare V3/Assets/Scripts/InventoryItemScript.cs
+++ Le Phare V3/Assets/Scripts/InventoryItemScript.cs
@@ -5,37 +5,63 @@ using UnityEngine.UI;
 
 public class InventoryItemScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
     [Header("UI")]
 
     [HideInInspector] public Image image;
     [HideInInspector] public Item item;
 
     Transform parentAfterDrag;
-    bool isDraging = false;
 
     public string CurrentItemIdName { get; set; }
 
-    //public InventorySlotScript[] inventorySlots;
+    Vector3 mouseWorldPosition;
+
+    private RectTransform rectTransform;
+
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Canvas canvas;
+
+    LayerMask layerMask;
 
     void Start()
     {
         image = GetComponent<Image>();
     }
 
+    void Awake()
+    {
+        //layerMask = LayerMask.GetMask("InteractibleObjects");
+        rectTransform = GetComponent<RectTransform>();
+
+        image = GetComponent<Image>();
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
+        //print(CurrentItemIdName);
 
-        print(CurrentItemIdName);
-        //image.raycastTarget = false;
         if (CurrentItemIdName != null)
         {
-            //isDraging = true;
             parentAfterDrag = transform.parent;
             transform.SetParent(transform.root);
             transform.SetAsLastSibling();
-
         }
     }
 
@@ -43,39 +69,54 @@ public class InventoryItemScript : MonoBehaviour, IBeginDragHandler, IDragHandle
     {
         if (CurrentItemIdName != null)
         {
-            isDraging = true;
-            transform.position = Input.mousePosition;
-            //isDraging = true;
+            //mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            //mouseWorldPosition.z = 0f;
+
+            ////print(mouseWorldPosition);
+            ////print(Input.mousePosition);
+            //mouseWorldPosition.z = 0f;
+
+            ////transform.position = Input.mousePosition;
+            //transform.position = mouseWorldPosition;
+
+            //Vector2 pos;
+            //RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            //    canvas.transform as RectTransform,
+            //    Input.mousePosition,
+            //    mainCamera,
+            //    out pos
+            //);
+
+            //rectTransform.anchoredPosition = pos;
+
+            Vector2 uiPosition;
+
+            // Convertit la position de la souris (écran) en position locale UI
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                canvas.transform as RectTransform,
+                Input.mousePosition,
+                mainCamera,
+                out uiPosition
+                );
+
+            rectTransform.anchoredPosition = uiPosition;
         }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        //image.raycastTarget = true;
+
         if (CurrentItemIdName != null)
         {
-            transform.SetParent(parentAfterDrag);
-            isDraging = false;
+            transform.SetParent(parentAfterDrag); 
         }
 
-        //isDraging = false;
-    }
 
-    void Awake()
-    {
-        image = GetComponent<Image>();
     }
 
     public void InitialiseItem(Item newItem)
     {
         item = newItem;
         image.sprite = newItem.image;
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (CurrentItemIdName == "cle" && isDraging == true)
-        {
-            GameObject.Destroy(gameObject);
-        }
     }
 }
