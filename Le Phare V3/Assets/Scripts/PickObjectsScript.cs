cfa0ff8b0905a80ab01abd74372a4c7106169088
Cloovee29 <clothilde.moulis@gmail.com>
2025-11-21 11:54:18 +0100

    Inventaire pick, utilisation, et suppression objet fonctionne

--- Le Phare V3/Assets/Scripts/PickObjectsScript.cs
+++ Le Phare V3/Assets/Scripts/PickObjectsScript.cs
@@ -7,21 +7,10 @@ public class PickObjectsScript : MonoBehaviour, IPointerClickHandler
     public InventoryManagerScript InventoryManagerScript;
     public Item itemToPickup;
 
-    //public Item[] itemsToPickup;
-
-    //public void OnPointerClick(PointerEventData eventData)
-    //{
-    //    PickupItem(0);
-    //}
-
-    //public void PickupItem(int id)
-    //{
-    //    InventoryManagerScript.AddItem(itemsToPickup[id]);
-    //}
-
     public void OnPointerClick(PointerEventData eventData)
     {
         PickupItem();
+        Destroy(gameObject);
     }
 
     public void PickupItem()
--- Le Phare V3/Assets/Scripts/targetObjectToUseScript.cs
+++ Le Phare V3/Assets/Scripts/targetObjectToUseScript.cs
@@ -2,15 +2,11 @@ using UnityEngine;
 
 public class targetObjectToUseScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-    public string targetName;
+    public ItemName targetName;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         
