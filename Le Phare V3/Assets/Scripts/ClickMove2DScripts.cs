49946a2b543b1ace35b925ed8a61eb3e13578053
Cloovee29 <clothilde.moulis@gmail.com>
2025-11-28 09:55:45 +0100

    dora bouge pas quand fleches

--- Le Phare V3/Assets/Scripts/ClickMove2DScripts.cs
+++ Le Phare V3/Assets/Scripts/ClickMove2DScripts.cs
@@ -1,27 +1,24 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ClickMove2D : MonoBehaviour
 {
-
-
     public float speed = 2f;  // vitesse 
     public float groundY = -1.06f;    // position sur axe des Y et hauteur du sol
     private Vector3 targetPosition;
 
     public GameObject Arrows;
-    Vector3 arrowsPosition;
-
-    Vector3 currentCharacterPosition;
+    public EventSystem eventSystem;
 
     void Start()
     {
         targetPosition = transform.position;   // début à son emplacement actuel sur scene (parler des emplacements des flèches qui dérangent le LD
-        Arrows.GetComponent<Transform>().position = arrowsPosition;
     }
 
     void Update()
     {        
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && eventSystem.currentSelectedGameObject == null)
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);   // position de mon curseur sur mon écran retranscris en position sur Unity
 
@@ -32,20 +29,11 @@ public class ClickMove2D : MonoBehaviour
             targetPosition = mousePos;
         }
 
-        Vector3 arrowsPos = Camera.main.ScreenToWorldPoint(arrowsPosition);
-
-        if (Input.GetMouseButtonDown(0) && Input.mousePosition == arrowsPos)
-        {
-            //transform.position = currentCharacterPos;
-            transform.position = currentCharacterPosition;
-        }
-
         transform.position = Vector3.MoveTowards(
 
         transform.position,
 
         targetPosition,
         speed * Time.deltaTime);
-
     }
 }
