5320a75318487c35b56320be8874ae819971e9a4
HaizheLeidyi <amelie.sept@gmail.com>
2025-11-28 08:26:30 +0100

    Create deplacement personnage
    Le perso bouge quand on clique et là où on clique

---
+++ Le Phare V3/Assets/Scripts/ClickMove2DScripts.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ClickMove2D : MonoBehaviour
+{
+
+
+    public float speed = 2f;  // vitesse 
+    public float groundY = -1.06f;    // position sur axe des Y et hauteur du sol
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        targetPosition = transform.position;   // début à son emplacement actuel sur scene (parler des emplacements des flèches qui dérangent le LD
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);   // position de mon curseur sur mon écran retranscris en position sur Unity
+
+            mousePos.y = groundY;
+
+            mousePos.z = 0;   // pas retirer sinon perso bouge sur les murs
+
+            targetPosition = mousePos;
+        }
+        transform.position = Vector3.MoveTowards(
+
+            transform.position,
+
+            targetPosition,
+            speed * Time.deltaTime
+        );
+    }
+}
