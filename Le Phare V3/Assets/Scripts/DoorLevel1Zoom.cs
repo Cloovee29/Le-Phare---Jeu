f1de90acf399d643726c8032eb3fa3a14fc3dea8
Cloovee29 <clothilde.moulis@gmail.com>
2025-12-02 18:51:14 +0100

    correction UI fond serrure

--- Le Phare V3/Assets/Scripts/DoorLevel1Zoom.cs
+++ Le Phare V3/Assets/Scripts/DoorLevel1Zoom.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class DoorLevel1Zoom : MonoBehaviour
 {
     public GameObject zoomSerrure;
+    public GameObject character;
+    public GameObject arrows;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,5 +20,7 @@ public class DoorLevel1Zoom : MonoBehaviour
     public void OpenDoor()
     {
         zoomSerrure.SetActive(true);
+        character.SetActive(false);
+        arrows.SetActive(false);
     }
 }
