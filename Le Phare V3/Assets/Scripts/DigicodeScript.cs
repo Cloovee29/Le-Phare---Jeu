72d180e4f2c926927393e88918944b4242a9e377
Sacha <noirjean.sacha@gmail.com>
2025-12-14 12:11:12 +0100

    Digicode fonctionnel partie 1

--- Le Phare V3/Assets/Scripts/DigicodeScript.cs
+++ Le Phare V3/Assets/Scripts/DigicodeScript.cs
@@ -5,29 +5,57 @@ public class DigicodeScript : MonoBehaviour
 {
     public GameObject zoomDigicode;
     public GameObject character;
-    public GameObject arrows;
+    public GameObject uiArrows;
+    public GameObject logoJournal;
 
-  
+    public GameObject chestOpen;
+    public GameObject chestClose;
+
+    public int goodAnswers;
+
     public void ButtonPressed(string tagNote)
     {
-        if (tagNote == "oui")
+        EnigmaEnd();
+
+        if (tagNote == "raven" || tagNote == "wave" || tagNote == "shell")
         {
-            print("enigme boite réussie");
             
+            goodAnswers = goodAnswers + 1;
+            print("bouton ok");
+            EnigmaEnd();
         }
         else
         {
             Debug.Log("Mauvaise touche");
         }
-
-
     }
+
+
+    public void EnigmaEnd()
+    {
+        if (goodAnswers == 3)
+        {
+            zoomDigicode.SetActive(false);
+            character.SetActive(true);
+            uiArrows.SetActive(true);
+            logoJournal.SetActive(true);
+
+            print("énigme finito pipo");
+            chestOpen.SetActive(true);
+            chestClose.SetActive(false);
+
+        }
+       
+    }
+
 
     public void OnMouseDown()
     {
         zoomDigicode.SetActive(true);
         character.SetActive(false);
-        arrows.SetActive(false);
+        logoJournal.SetActive(false);
+        uiArrows.SetActive(false);
+        
     }
   
 }
