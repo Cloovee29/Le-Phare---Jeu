693e344c11c5fdfb1159e9ef90cac5b9158f2975
HaizheLeidyi <amelie.sept@gmail.com>
2025-12-30 21:10:16 +0100

    Sons du journal open et flip pages

---
+++ Le Phare V3/Assets/Scripts/SoundDesignScripts/JournalSoundController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class JournalAudioManager : MonoBehaviour
+{
+    public static JournalAudioManager Instance; // singleton
+
+    public AudioClip openSound;
+    //public AudioClip closeSound;
+    public AudioClip pageTurn;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    public void PlayOpenSound()
+    {
+        audioSource.PlayOneShot(openSound);
+    }
+
+    //public void PlayCloseSound()
+    //{
+    //    audioSource.PlayOneShot(closeSound);
+    //}
+
+    public void PlayPageTurn()
+    {
+        audioSource.PlayOneShot(pageTurn);
+    }
+}
